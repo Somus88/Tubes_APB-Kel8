59e9c420d745e8301326d0206107ef658a6d51a3
Somus88 <96776245+Somus88@users.noreply.github.com>
2025-06-16 13:39:14 +0700

    Delete tubes_progress directory

--- tubes_progress/android/app/src/main/kotlin/com/example/tubes_progress/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tubes_progress
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- tubes_progress/computing/android/app/src/main/kotlin/com/example/computing/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.computing
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
